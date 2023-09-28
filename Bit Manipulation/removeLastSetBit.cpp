1536efddbe5da77f7947386d4917e69754746def
tauseef9580 <97532000+tauseef9580@users.noreply.github.com>
2023-09-29 00:15:38 +0530

    Create removeLastSetBit.cpp

---
+++ Bit Manipulation/removeLastSetBit.cpp
@@ -0,0 +1,34 @@
+/*
+
+  Question :- remove the last set bit of a given number
+
+-------Approach--------
+  to remove that set bit first we have to find out which bit is 1 if we start from the lest side, when we encounter that bit, we will remove that
+the code is provided below
+
+*/
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    
+    int mask = 1;
+    while(mask <= n){ // till mask is less then number
+        if((mask & n) == mask){ // if & of n and mask is equal to mask itself that mean that bit is 1 where mask bit is 1
+            n = mask^n; // we will remove that bit by this 
+            break;
+        }
+        //else
+        mask = mask<<1; // till we don't find out that bit , keep moving mask bitby 1 bit left 
+    }
+    
+    cout<<n<<endl;
+    return 0;
+}
